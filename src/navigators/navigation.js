52093cd4bda3c9c9724fa7c3ec7c675aa334d215
Ulas Ozturk <ozulashe@gmail.com>
2024-01-15 10:10:20 +0300

    App icon and splash screen created.

--- src/navigators/navigation.js
+++ src/navigators/navigation.js
@@ -3,13 +3,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {MainStack} from './mainStack';
 import {AuthStack} from './authStack';
 import {useInitialRenewToken, useIsAuthenticated} from '../redux/reducers/auth';
+import SplashScreen from 'react-native-splash-screen';
 
 export function Navigation() {
   const isAuthChecking = useInitialRenewToken();
   const isAuthenticated = useIsAuthenticated();
   const [isNavigationReady, setIsNavigationReady] = React.useState(false);
 
-  // TODO: show splash screen
+  React.useEffect(() => {
+    if (!isAuthChecking && isNavigationReady) {
+      SplashScreen.hide();
+    }
+  }, [isNavigationReady]);
 
   return (
     <NavigationContainer onReady={() => setIsNavigationReady(true)}>
